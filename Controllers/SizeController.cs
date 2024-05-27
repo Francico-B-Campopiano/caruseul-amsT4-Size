625b1e9b9017403e108d1fba3e340b7ecfaf1759
Francico-B-Campopiano <163892385+Francico-B-Campopiano@users.noreply.github.com>
2024-05-27 16:16:55 -0300

    Add files via upload

---
+++ Controllers/SizeController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AMST4.Carousel.Context;
+using AMST4.Carousel.Models;
+
+namespace AMST4.Carousel.Controllers
+{
+    public class SizeController : Controller
+    {
+        private readonly ApplicationDataContext _context;
+
+        public SizeController(ApplicationDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Size
+        public IActionResult Index()
+        {
+            var size = _context.Size.ToList();
+            return View(size);
+
+        }
+
+
+        // GET: Size/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Size/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        
+        public IActionResult Create(Size size)
+        {
+            if (ModelState.IsValid)
+            {
+                size.Id = Guid.NewGuid();
+                _context.Add(size);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(size);
+        }
+
+        // GET: Size/Edit/5
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = _context.Size.Find(id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+            return View(size);
+        }
+
+        // POST: Size/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        
+        public IActionResult Edit(Guid id,Size size)
+        {
+            if (id != size.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(size);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SizeExists(size.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(size);
+        }
+
+        // GET: Size/Delete/5
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size =  _context.Size
+                .FirstOrDefault(m => m.Id == id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+
+            return View(size);
+        }
+
+        // POST: Size/Delete/5
+        [HttpPost, ActionName("Delete")]
+        
+        public IActionResult DeleteConfirmed(Guid id)
+        {
+            var size = _context.Size.Find(id);
+            if (size != null)
+            {
+                _context.Size.Remove(size);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SizeExists(Guid id)
+        {
+            return _context.Size.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Size.cs
@@ -0,0 +1,11 @@
+﻿namespace AMST4.Carousel.Models
+{
+    public class Size
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Created { get; set; }
+
+    }
+}
